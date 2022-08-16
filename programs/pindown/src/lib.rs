cb23732631a4682f7bac9f2a8f954fb626dbe785
aswinraj <aswinrajkailath@gmail.com>
2022-08-16 23:25:28 +0530

    pindown deployed devnet

--- programs/pindown/src/lib.rs
+++ programs/pindown/src/lib.rs
@@ -1,15 +1,66 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FAU7uzegHAhsV8NYZ1BXzmCaAW8vVTKnNfknNgQcFB9u");
 
 #[program]
 pub mod pindown {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    
+    base_account.total_docs = 0;
+    Ok(())
+  }
+    
+    pub fn add_doc(ctx: Context<AddDoc>, doc_link: String, dest_addr: String ) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+    
+        // Build the struct.
+        let item = ItemStruct {
+          doc_link: doc_link.to_string(),
+          dest_addr: dest_addr.to_string(),
+          user_address: *user.to_account_info().key,
+        };
+            
+        // Add it to the doc_list vector.
+        base_account.doc_list.push(item);
+        base_account.total_docs += 1;
         Ok(())
-    }
+      }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+// Specify what data you want in the AddDoc Context.
+
+#[derive(Accounts)]
+pub struct AddDoc<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub doc_link: String,
+    pub dest_addr: String,
+    pub user_address: Pubkey,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_docs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub doc_list: Vec<ItemStruct>,
+}
--- tests/pindown.js
+++ tests/pindown.js
@@ -1,13 +1,51 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
-describe("pindown", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Pindown;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Pindown;
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+  console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Document Count', account.totalDocs.toString())
+
+  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
+  await program.rpc.addDoc("https://media.giphy.com/media/gPzqtWuOtkDbDEDoXE/giphy.gif", "testaddress", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Document Count', account.totalDocs.toString())
+
+  
+  console.log('👀 Document List', account.docList)
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
